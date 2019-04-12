752ffad1e81789fb2b4dbee0f591e1b8738aa776
Perscholas40 <todd_wilcher@PSADMINs-MacBook-Air-49.local>
2019-04-12 11:26:57 -0400

    Dealer / Player Hand logic

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 // Instructions Modal
 
-document.getElementById('.titleBox').addEventListener("click",
-function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-});
-document.querySelector(".close").addEventListener("click", function() {
-    document.querySelector('.bg-modal').style.display = 'none';
-})
+// document.getElementById('.titleBox').addEventListener("click",
+// function(){
+//     document.querySelector('.bg-modal').style.display = 'flex';
+// });
+// document.querySelector(".close").addEventListener("click", function() {
+//     document.querySelector('.bg-modal').style.display = 'none';
+// })
+
+
+//Card Deck
+
+var cardDeck = [2,3,4,5,6,7,8,9,10,10,10,10,11,
+                2,3,4,5,6,7,8,9,10,10,10,10,11,
+                2,3,4,5,6,7,8,9,10,10,10,10,11,
+                2,3,4,5,6,7,8,9,10,10,10,10,11,]
+
+// console.log(cardDeck[0], cardDeck[52])
+
+function drawCard(cardDeck){
+    let randomCards = Math.floor(cardDeck.length * Math.random());
+    return cardDeck[randomCards];
+}
+
+function gameBegin(){
+    playerHand = [drawCard(cardDeck), drawCard(cardDeck)]
+    dealerHand = [drawCard(cardDeck), drawCard(cardDeck)]
+}
+gameBegin();
+console.log('Player Hand: ' + playerHand);
+console.log('Dealer Hand: ' + dealerHand)
+// let playerHand = [];
+// let dealerHand = [];
+console.log(drawCard(cardDeck));
