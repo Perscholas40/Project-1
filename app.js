ba3faa219a1e598bbecceec640c95f343029dd2d
Perscholas40 <todd_wilcher@PSADMINs-MacBook-Air-49.local>
2019-04-12 12:26:48 -0400

    function for adding cards in hand

--- app.js
+++ app.js
@@ -9,27 +9,44 @@
 // })
 
 
-//Card Deck
+//Card Deck & Variables
 
 var cardDeck = [2,3,4,5,6,7,8,9,10,10,10,10,11,
                 2,3,4,5,6,7,8,9,10,10,10,10,11,
                 2,3,4,5,6,7,8,9,10,10,10,10,11,
                 2,3,4,5,6,7,8,9,10,10,10,10,11,]
+var playerHand;
+var dealerHand;
 
 // console.log(cardDeck[0], cardDeck[52])
 
+// Grabs random cards from card deck array
 function drawCard(cardDeck){
     let randomCards = Math.floor(cardDeck.length * Math.random());
     return cardDeck[randomCards];
 }
-
+//Makes beginning hand for dealer and player
 function gameBegin(){
     playerHand = [drawCard(cardDeck), drawCard(cardDeck)]
     dealerHand = [drawCard(cardDeck), drawCard(cardDeck)]
 }
+
+// Gets Value of current hand
+function handTotal(hand){
+    var total = 0;
+    for(var i = 0; i < hand.length; i++){
+        total = total + hand[i];
+    }
+    return total;
+}
+
+
+
+
 gameBegin();
 console.log('Player Hand: ' + playerHand);
+console.log('Player Hand total: ' + handTotal(playerHand))
 console.log('Dealer Hand: ' + dealerHand)
+console.log('Dealer Hand total: ' + handTotal(dealerHand))
 // let playerHand = [];
 // let dealerHand = [];
-console.log(drawCard(cardDeck));
