250840d61ea00e82a5f2649c39419a95cb28e2a1
Perscholas40 <todd_wilcher@PSADMINs-MacBook-Air-49.local>
2019-04-11 22:18:20 -0400

    new themed pages added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-document.getElementById("dealerHand")
+// Instructions Modal
+
+document.getElementById('.titleBox').addEventListener("click",
+function(){
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+})
